360c2b4c59806ade3d9a9ed531d6d849cd1cccbb
datlv <datle2011@gmail.com>
2018-10-01 15:21:52 +0700

    fix

--- remote-install-chaincode.sh
+++ remote-install-chaincode.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 ssh -p 22 -i /home/datlv/Documents/deevo/key/dev-full-rights.pem ubuntu@54.169.140.0 ' cd /opt/gopath/src/github.com/deevotech/supply-chain-network/scripts/ && export FABRIC_CFG_PATH=/home/ubuntu/hyperledgerconfig/data/ && bash ./chaincode-caliper.sh -c mychannel -n simple -v 1.0 ; sleep 3; exit; '
+ssh -p 22 -i /home/datlv/Documents/deevo/key/dev-full-rights.pem ubuntu@54.169.140.0 ' cd /opt/gopath/src/github.com/deevotech/supply-chain-network/scripts/ && export FABRIC_CFG_PATH=/home/ubuntu/hyperledgerconfig/data/ && bash ./chaincode-mvp.sh -c mychannel -n supplychain -v 1.0 ; sleep 3; exit; '
