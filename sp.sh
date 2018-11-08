a1462b7d6c048e993a6d53a95c99cd852588adbb
datlv <datle2011@gmail.com>
2018-11-08 08:35:43 +0700

    fix

--- sp.sh
+++ sp.sh
@@ -26,11 +26,12 @@ n=6 ; eval a$n="18.136.104.14"
 n=7 ; eval a$n="13.250.112.83"
 n=8 ; eval a$n="52.221.181.66"
 n=9 ; eval a$n="54.169.140.0"
-n=10 ; eval a$n="18.136.205.13"
-n=11 ; eval a$n="52.77.238.1"
-n=12 ; eval a$n="13.250.109.150"
-n=13 ; eval a$n="54.251.183.191"
-n=14 ; eval a$n="52.221.179.90"
+n=10 ; eval a$n="18.136.205.13" # api.deevo.io
+n=11 ; eval a$n="52.77.238.1" # insight.deevo.io
+n=12 ; eval a$n="13.250.109.150" # status.deevo.io
+n=13 ; eval a$n="54.251.183.191" # sc.deevo.io
+n=14 ; eval a$n="52.221.179.90" # lina.deevo.io
+n=15 ; eval a$n="52.221.179.90" # benchmark.deevo.io
 eval ip=\$a$i
 echo ${ip}
 ssh -p 22 -i /home/datlv/Documents/deevo/key/dev-full-rights.pem ubuntu@${ip}
